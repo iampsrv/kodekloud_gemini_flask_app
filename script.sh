685fcf3dd37781803a309afeba1afeda606181b7
Pranjal Srivastava <75536136+iampsrv@users.noreply.github.com>
2024-01-25 17:30:37 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,6 @@
+kubectl create -f namespace_prod.yaml
+kubectl create -f namespace_staging.yaml
+kubectl create -f secret_prod.yaml
+kubectl create -f secret_staging.yaml
+kubectl create -f service_prod.yaml
+kubectl create -f service_staging.yaml
